a111ecee0ce1aef7580f8a6e42482f3c3c1b8c4f
Dewmina Udayashan <dewminaudayashan@gmail.com>
2022-09-08 14:34:21 +0530

    form creation

--- ASP_winform_demo/Form1.Designer.cs
+++ ASP_winform_demo/Form1.Designer.cs
@@ -28,12 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nameField = new System.Windows.Forms.TextBox();
+            this.ageField = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.showNameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(31, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(31, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Age";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // nameField
+            // 
+            this.nameField.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.nameField.Location = new System.Drawing.Point(114, 50);
+            this.nameField.Name = "nameField";
+            this.nameField.Size = new System.Drawing.Size(232, 27);
+            this.nameField.TabIndex = 2;
+            this.nameField.UseWaitCursor = true;
+            // 
+            // ageField
+            // 
+            this.ageField.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.ageField.Location = new System.Drawing.Point(114, 107);
+            this.ageField.Name = "ageField";
+            this.ageField.Size = new System.Drawing.Size(232, 27);
+            this.ageField.TabIndex = 3;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(158, 192);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(139, 29);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // showNameLabel
+            // 
+            this.showNameLabel.AutoSize = true;
+            this.showNameLabel.Location = new System.Drawing.Point(19, 413);
+            this.showNameLabel.Name = "showNameLabel";
+            this.showNameLabel.Size = new System.Drawing.Size(0, 20);
+            this.showNameLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.showNameLabel);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.ageField);
+            this.Controls.Add(this.nameField);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox nameField;
+        private TextBox ageField;
+        private Button saveButton;
+        private Label showNameLabel;
     }
 }
